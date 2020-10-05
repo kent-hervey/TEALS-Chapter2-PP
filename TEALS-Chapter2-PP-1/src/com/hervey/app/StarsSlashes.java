0dffc0eb08dc2bf450832161e07984db61191fa1
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-10-05 16:26:00 -0500

    small cleanup

--- TEALS-Chapter2-PP-1/src/com/hervey/app/StarsSlashes.java
+++ TEALS-Chapter2-PP-1/src/com/hervey/app/StarsSlashes.java
@@ -3,18 +3,15 @@ package com.hervey.app;
 public class StarsSlashes {
 
 	public static void main(String[] args) {
-
 		StarsSlashes.drawStarsSlashes();
-
 	}
 
 	private static void drawStarsSlashes() {
-		//Seven lines, 24 characters wide
-		int numberLines=7;
-		int totalCharWidth=24;
-		
-		//Outer loop handles each line; i indicates current line
-		for(int i=1; i <=numberLines; i++) {
+		// Seven lines, 24 characters wide
+		int numberLines = 7;
+
+		// Outer loop handles each line; i indicates current line
+		for (int i = 1; i <= numberLines; i++) {
 			int numStars = numberLines - i;
 			int numSpaces = i - 1;
 			int numForwardSlashes = numStars * 2;
@@ -25,37 +22,36 @@ public class StarsSlashes {
 			StarsSlashes.drawBackSlashesQty(numBackSlashes);
 			StarsSlashes.drawSpacesQty(numSpaces);
 			StarsSlashes.drawStarsQty(numStars);
-			
+
 			System.out.print("\n");
 		}
-		
-		
+
 	}
 
 	private static void drawBackSlashesQty(int numBackSlashes) {
-		for(int j=0; j < numBackSlashes; j++) {
+		for (int j = 0; j < numBackSlashes; j++) {
 			System.out.print("\\");
 		}
-		
+
 	}
 
 	private static void drawForwardSlashesQty(int numForwardSlashes) {
-		for(int j=0; j < numForwardSlashes; j++) {
+		for (int j = 0; j < numForwardSlashes; j++) {
 			System.out.print("/");
 		}
 	}
 
 	private static void drawSpacesQty(int numSpaces) {
-		for(int j=0; j < numSpaces; j++) {
+		for (int j = 0; j < numSpaces; j++) {
 			System.out.print(" ");
-	}
+		}
 	}
 
 	private static void drawStarsQty(int numStars) {
-		for(int j=0; j < numStars; j++) {
+		for (int j = 0; j < numStars; j++) {
 			System.out.print("*");
 		}
-		
+
 	}
 
 }
